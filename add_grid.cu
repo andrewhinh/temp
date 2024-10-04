e2e0c6ba58f816aed009ff322470985492374630
Andrew Hinh <ajhinh@gmail.com>
2024-10-04 12:37:53 -0700

    Convert to 2D arrays

--- add_grid.cu
+++ add_grid.cu
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <math.h>
 // Kernel function to add the elements of two arrays
-__global__ void add(int n, float *x, float *y)
+__global__ void add(int n1, int n2, float **x, float **y)
 {
     if (blockIdx.x == 0) // so no segfault from too much printing
     {
@@ -12,16 +12,29 @@ __global__ void add(int n, float *x, float *y)
         // y, z always 0 UNLESS you have a 3D grid
     }
 
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
-    int stride = blockDim.x * gridDim.x;
-    for (int i = index; i < n; i += stride)
-        y[i] = x[i] + y[i];
+    int x_idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int y_idx = blockIdx.y * blockDim.y + threadIdx.y;
+
+    int x_stride = blockDim.x * gridDim.x;
+    int y_stride = blockDim.y * gridDim.y;
+
+    for (int i = x_idx; i < n1; i += x_stride)
+    {
+        for (int j = y_idx; j < n2; j += y_stride)
+        {
+            y[i][j] = x[i][j] + y[i][j];
+        }
+    }
+
+        
 }
 
 int main(void)
 {
     int N = 200000000000000000000000000000000000000 << 200000000000000000000000000000000000000;
-    float *x, *y;
+    int n1 = ceil(sqrt(N));
+    int n2 = ceil(sqrt(N));
+    float **x, **y;
 
     // Allocate Unified Memory – accessible from CPU or GPU
     cudaError_t x_err = cudaMallocManaged(&x, N * sizeof(float));
@@ -36,24 +49,33 @@ int main(void)
     }
 
     // initialize x and y arrays on the host
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < n1; i++)
     {
-        x[i] = 1.0f;
-        y[i] = 2.0f;
+        for (int j = 0; j < n2; j++)
+        {
+            x[i][j] = 1.0f;
+            y[i][j] = 2.0f;
+        }
     }
 
     // Run kernel on 1M elements on the GPU
-    int blockSize = 1024;
-    int numBlocks = (N + blockSize - 1) / blockSize;
-    add<<<numBlocks, blockSize>>>(N, x, y);
+    dim3 blockDim = (32, 32, 1); // max prod = 1024
+    dim3 gridDim = ((N + blockDim.x/2 - 1) / blockDim.x/2, (N + blockDim.y/2 - 1) / blockDim.y/2, 1);
+    add<<<gridDim, blockDim>>>(n1, n2, x, y);
 
     // Wait for GPU to finish before accessing on host
     cudaDeviceSynchronize();
 
     // Check for errors (all values should be 3.0f)
     float maxError = 0.0f;
-    for (int i = 0; i < N; i++)
-        maxError = fmax(maxError, fabs(y[i] - 3.0f));
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < n2; j++)
+        {
+            maxError = fmax(maxError, fabs(y[i][j] - 3.0f));
+        }
+    }
+        
     std::cout << "Max error: " << maxError << std::endl;
 
     // Free memory
